204c9b5648004288b504198d96c9f26d9db7c602
Nirmal Mekala <you@example.com>
2022-06-22 13:13:32 -0500

    getting acclamated to konva

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './App.css';
+import { Stage, Layer, Rect, Circle, Text } from 'react-konva';
+import Konva from 'konva';
+
+class ColoredRect extends React.Component {
+  state = {
+    color: "cadetblue"
+  };
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <Circle
+        x={50}
+        y={50}
+        width={50}
+        height={50}
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={this.handleClick}
+        draggable={true}
+      />
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hi there ;)</p>
+        <Stage width={500} height={500}>
+          <Layer>
+            <ColoredRect />
+          </Layer>
+        </Stage>
      </header>
     </div>
   );
