77e3212a12b9edc334cf1b06b9730dadd769d466
Nirmal Mekala <ndmekala@icloud.com>
2022-06-28 22:07:24 -0500

    convert to hooks, add rudimentary smart arrow positioning

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Stage, Ellipse, Group, Arrow, Line, Layer, Circle, Text, useStrictMode,
 import Konva from 'konva';
 
 useStrictMode(true)
-// the TLDR is that my move to mix up hooks and class components is somewhere along the spectrum from tenuous to sketchy to unsuported. try to refactor with more consistent hook usage would be my best guess.
+
 const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) =>  {
   const shapeRef = React.useRef();
   const trRef = React.useRef();
@@ -71,75 +71,43 @@ const rectData = [{
   id: 'rect1'
 }]
 
-class App extends React.Component {
-  constructor() {
-    super();
+const App = () => {
+  const [c1, setC1] = React.useState([50, 50])
+  const [c2, setC2] = React.useState([150, 150])
+  const [c3, setC3] = React.useState([250, 250])
+  const [c4, setC4] = React.useState([425, 425])
+  const [cirkows, setCirkows] = React.useState([[10, 400], [10,300]])
+  const [rectangles, setRectangles] = React.useState(rectData)
+  const [selectedID, setSeletedID] = React.useState(null)
 
-    this.state = {
-      c1x: 50,
-      c1y: 50,
-      c2x: 150,
-      c2y: 150,
-      c3x: 250,
-      c3y: 250,
-      c4x: 425,
-      c4y: 425,
-      cirkows: [[10,400],[10,300]],
-      rectangles: rectData,
-      selectedID: null,
-    }
+  const handleDrop = (event) => {
 
-//    this.updatePos = this.updatePos.bind(this);
-    this.update = this.update.bind(this);
-    this.handleDrop = this.handleDrop.bind(this);
-
-  }
-
-  handleDrop(event) {
     if (event.target.x() < 400) {
-      console.log('this is here')
-      let stateCopy = this.state.cirkows
+      let stateCopy = cirkows
       stateCopy.push([event.target.x(), event.target.y()])
-      this.setState({cirkows: stateCopy})
-    }
+      setCirkows(stateCopy)
+      }
+
   }
 
-  update(circ, xPos, yPos) {
-    if (circ === 1) {
-      this.setState({
-        c1x: xPos,
-        c1y: yPos,
-      })
-    }
-    if (circ === 2) {
-      this.setState({
-        c2x: xPos,
-        c2y: yPos,
-      })
-    }
-    if (circ === 3) {
-      this.setState({
-        c3x: xPos,
-        c3y: yPos,
-      })
-    }
-    if (circ === 4) {
-      this.setState({
-        c4x: xPos,
-        c4y: yPos,
-      })
-    }
+  const getAdjustments = (x1, y1, x2, y2, r) => {
+    let slope = (y2-y1)/(x2-x1)
+    let xAdj1 = Math.sqrt(r*r/(1+(slope*slope)))
+    let yAdj1 = slope*xAdj1
+    let xAdj2 = -xAdj1
+    let yAdj2 = -yAdj1
+    let results = [xAdj1, yAdj1, xAdj2, yAdj2];
+    console.log(results)
+    return results
   }
 
-  render() {
-
-  return (
+    return (
     <div className="App">
       <header className="App-header">
         <p>Hi there 🙃</p>
         <Stage width={500} height={500} style={{border: '2px solid deeppink'}}>
           <Layer>
-	  <Circle radius={15} fill='deeppink' x={420} y={400} draggable={true} onDragEnd={(e) => {this.handleDrop(e)}}/>
+		  {/*<Circle radius={15} fill='deeppink' x={420} y={400} draggable={true} onDragEnd={(e) => {this.handleDrop(e)}}/> */}
 	  </Layer>
           <Layer>
             {/*this.state.rectangles.map((rect, i) => {
@@ -162,7 +130,7 @@ class App extends React.Component {
 		}}
 		/>)
 	    })*/} 
-		  <Rectangle height={50} width={50} fill='deeppink' />
+		  {/*<Rectangle height={50} width={50} fill='deeppink' />*/}
 
             <Group
               draggable={true}>
@@ -183,12 +151,14 @@ class App extends React.Component {
 		text='yo!'
 	      />
             </Group>
-	    {this.state.cirkows.map((cirkow) => <Circle width={15} height={15} fill='deeppink' x={cirkow[0]} y={cirkow[1]} /> )}
-	    <Circle width={50} height={50} fill='cadetblue' draggable={true} x={this.state.c1x} y={this.state.c1y} onDragMove={(e) => {this.update(1, e.target.x(), e.target.y())}}/>
-	    <Circle width={50} height={50} fill='cadetblue' draggable={true} x={this.state.c2x} y={this.state.c2y} onDragMove={(e) => {this.update(2, e.target.x(), e.target.y())}}/>
-	    <Circle width={50} height={50} fill='cadetblue' draggable={true} x={this.state.c3x} y={this.state.c3y} onDragMove={(e) => {this.update(3, e.target.x(), e.target.y())}}/>
-	    <Arrow stroke='cadetblue' points={[this.state.c1x+25,this.state.c1y+25,this.state.c2x-25,this.state.c2y-25]} />
-	    <Arrow stroke='cadetblue' points={[this.state.c2x+25,this.state.c2y+25,this.state.c3x-25,this.state.c3y-25]} />
+	    {cirkows.map((cirkow) => <Circle width={15} height={15} fill='deeppink' x={cirkow[0]} y={cirkow[1]} /> )}
+	    <Circle radius={20} fill='cadetblue' draggable={true} x={c1[0]} y={c1[1]} onDragEnd={(e) => {setC1([e.target.x(), e.target.y()])}}/>
+	    <Circle radius={20} fill='cadetblue' draggable={true} x={c2[0]} y={c2[1]} onDragEnd={(e) => {setC2([e.target.x(), e.target.y()])}}/>
+	    <Circle radius={20} fill='cadetblue' draggable={true} x={c3[0]} y={c3[1]} onDragEnd={(e) => {setC3([e.target.x(), e.target.y()])}}/>
+	    <Arrow stroke='cadetblue' points={[c1[0]+25,c1[1]+25,c2[0]-25,c2[1]-25]} />
+	    <Arrow stroke='cadetblue' points={[c2[0]+getAdjustments(c2[0],c2[1],c3[0],c3[1],25)[0],c2[1]+getAdjustments(c2[0],c2[1],c3[0],c3[1],25)[0],c3[0]+getAdjustments(c2[0],c2[1],c3[0],c3[1],25)[2],c3[1]+getAdjustments(c2[0],c2[1],c3[0],c3[1],25)[3]]} />
+	    {/*<Arrow stroke='cadetblue' points={[this.state.c1x+25,this.state.c1y+25,this.state.c2x-25,this.state.c2y-25]} />*/}
+	    {/*<Arrow stroke='cadetblue' points={[this.state.c2x+25,this.state.c2y+25,this.state.c3x-25,this.state.c3y-25]} />*/}
 	    <Line stroke='deeppink' points={[400,0,400,500]} />
           </Layer>
         </Stage>
@@ -197,7 +167,71 @@ class App extends React.Component {
   );
 
 
-  }
 }
+
+
+//class App extends React.Component {
+//  constructor() {
+//    super();
+
+//    this.state = {
+//      c1x: 50,
+//      c1y: 50,
+//      c2x: 150,
+//      c2y: 150,
+//      c3x: 250,
+//      c3y: 250,
+//      c4x: 425,
+//      c4y: 425,
+//      cirkows: [[10,400],[10,300]],
+//      rectangles: rectData,
+//      selectedID: null,
+//    }
+
+//    this.update = this.update.bind(this);
+//    this.handleDrop = this.handleDrop.bind(this);
+//
+//  }
+
+//  handleDrop(event) {
+//    if (event.target.x() < 400) {
+//      console.log('this is here')
+//      let stateCopy = this.state.cirkows
+//      stateCopy.push([event.target.x(), event.target.y()])
+//      this.setState({cirkows: stateCopy})
+//    }
+//  }
+// change how this actually works
+//  update(circ, xPos, yPos) {
+//    if (circ === 1) {
+//      this.setState({
+//        c1x: xPos,
+//        c1y: yPos,
+//      })
+//    }
+//    if (circ === 2) {
+//      this.setState({
+//        c2x: xPos,
+//        c2y: yPos,
+//      })
+//    }
+//    if (circ === 3) {
+//      this.setState({
+//        c3x: xPos,
+//        c3y: yPos,
+//      })
+//    }
+//    if (circ === 4) {
+//      this.setState({
+//        c4x: xPos,
+//        c4y: yPos,
+//      })
+//    }
+//  }
+
+//  render() {
+
+//  }
+//}
 
 export default App;
