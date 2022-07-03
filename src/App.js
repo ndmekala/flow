3e9451299d3a562695e57ff184871ac3a55dc333
Nirmal Mekala <ndmekala@icloud.com>
2022-07-03 05:31:51 -0500

    continuing to build out node compoonent and adding comments

--- src/App.js
+++ src/App.js
@@ -1,79 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Stage, Ellipse, Group, Arrow, Line, Layer, Circle, Text, useStrictMode, Transformer, Rect} from 'react-konva';
+import { Stage, Group, Arrow, Line, Layer, Circle, Text, useStrictMode, Transformer} from 'react-konva';
 import { Html } from 'react-konva-utils';
 
 useStrictMode(true)
 // MAKE IT SO STATE ONLY UPDATES ON DRAG END!!! (this will help undo/redo)
 // or add a ~layer~ between state and dragging…
 // it could be isDragging ? x = yadayada : x = state value
-const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) =>  {
-  const shapeRef = React.useRef();
-  const trRef = React.useRef();
 
-  React.useEffect(() => {
-    if (isSelected) {
-      trRef.current.nodes([shapeRef.current]);
-      trRef.current.getLayer().batchDraw();
-    }
-  }, [isSelected]);
 
-  return (
-    <React.Fragment>
-      <Rect
-        onClick={onSelect}
-        onTap={onSelect}
-        ref={shapeRef}
-        {...shapeProps}
-        draggable
-        onDragEnd={(e) => {
-          onChange({
-            ...shapeProps,
-            x: e.target.x(),
-            y: e.target.y(),
-          })
-        }}
-        onTransformerEnd={(e) => {
-          const node = shapeRef.current;
-          const scaleX = node.scaleX();
-          const scaleY = node.scaleY();
-          node.scaleX(1);
-          node.scaleY(1);
-          onChange({
-            ...shapeProps,
-            x: node.x(),
-            y: node.y(),
-            width: Math.max(5, node.width() * scaleX),
-            height: Math.max(node.height() * scaleY),
-          });
-        }}
-      />
-      {isSelected && (
-        <Transformer
-          ref={trRef}
-          boundBoxFunc={(oldBox, newBox) => {
-            if(newBox.width < 5 || newBox.height < 5) {
-              return oldBox
-            }
-            return newBox;
-          }}
-        />
-      )}
-    </React.Fragment>
-  )
-
-}
-
-const rectData = [{
-  x: 250,
-  y: 300,
-  width: 100,
-  height: 100,
-  fill: 'deeppink',
-  id: 'rect1'
-}]
-
-const CircleComp = ({ shapeProps, isSelected, onSelect, onChange }) =>  {
+const Node = ({ shapeProps, isSelected, onSelect, onChange }) =>  {
   const shapeRef = React.useRef();
   const trRef = React.useRef();
 
@@ -87,58 +23,58 @@ const CircleComp = ({ shapeProps, isSelected, onSelect, onChange }) =>  {
   return (
     <React.Fragment>
       <Group draggable>
-      <Circle
-        onClick={onSelect}
-        onTap={onSelect}
-        ref={shapeRef}
-        {...shapeProps}
-        onDragEnd={(e) => {
-          onChange({
-            ...shapeProps,
-            x: e.target.x(),
-            y: e.target.y(),
-          })
-        }}
-        onTransformerEnd={(e) => {
-          const node = shapeRef.current;
-          const scaleX = node.scaleX();
-          const scaleY = node.scaleY();
-          node.scaleX(1);
-          node.scaleY(1);
-          onChange({
-            ...shapeProps,
-            x: node.x(),
-            y: node.y(),
-            width: Math.max(5, node.width() * scaleX),
-            height: Math.max(node.height() * scaleY),
-          });
-        }}
-      />
-      {isSelected && (
-        <Transformer
-          ref={trRef}
-          rotateEnabled={false}
-          centeredScaling={true}
-          keepRatio={true}
-          enabledAnchors={['top-right', 'bottom-right', 'top-left', 'bottom-left']}
-          boundBoxFunc={(oldBox, newBox) => {
-            if(newBox.width < 5 || newBox.height < 5) {
-              return oldBox
-            }
-            return newBox;
+        <Circle
+          onClick={onSelect}
+          onTap={onSelect}
+          ref={shapeRef}
+          {...shapeProps}
+          onDragEnd={(e) => {
+            onChange({
+              ...shapeProps,
+              x: e.target.x(),
+              y: e.target.y(),
+            })
+          }}
+          onTransformerEnd={(e) => {
+            const node = shapeRef.current;
+            const scaleX = node.scaleX();
+            const scaleY = node.scaleY();
+            node.scaleX(1);
+            node.scaleY(1);
+            onChange({
+              ...shapeProps,
+              x: node.x(),
+              y: node.y(),
+              width: Math.max(5, node.width() * scaleX),
+              height: Math.max(node.height() * scaleY),
+            });
           }}
         />
-      )}
-      <Text
-        x={400}
-        y={350}
-        fill='white'
-        fontFamily='Courier New'
-        text='yo!'
-      />
+        {isSelected && (
+          <Transformer
+            ref={trRef}
+            rotateEnabled={false}
+            centeredScaling={true}
+            keepRatio={true}
+            enabledAnchors={['top-right', 'bottom-right', 'top-left', 'bottom-left']}
+            boundBoxFunc={(oldBox, newBox) => {
+              if(newBox.width < 5 || newBox.height < 5) {
+                return oldBox
+              }
+              return newBox;
+            }}
+          />
+        )}
+        <Text
+          x={400}
+          y={350}
+          fill='white'
+          fontFamily='Courier New'
+          text='yo!'
+        />
       </Group>
       {/* I say its open for debate whether to use group or have text x y update via state */}
-      <Arrow stroke='chartreuse' fill='chartreuse' points={[0,0,500,500]}/>
+      <Arrow stroke='chartreuse' fill='chartreuse' points={[500,0,0,500]}/>
     </React.Fragment>
   )
 
@@ -149,13 +85,17 @@ const nodeData = [{
   y: 400,
   radius: 50,
   fill: 'cadetblue',
-  id: 'node1'
+  id: 'node1',
+  descendents: [{
+    id: 'node2',
+    arrowCoeff: 0.5,
+  }]
 }]
 
 
 const moreShapesData = [
   {
-    id: 1, // not actually in use…
+    id: 1, // not actually in use…?
     x: 150,
     y: 50,
     descendents: []
@@ -191,8 +131,6 @@ const App = () => {
   const [c1, setC1] = React.useState([50, 50])
   const [c2, setC2] = React.useState([150, 150])
   const [c3, setC3] = React.useState([250, 250])
-  const [cirkows, setCirkows] = React.useState([])
-  const [rectangles, setRectangles] = React.useState(rectData)
   const [nodes, setNodes] = React.useState(nodeData)
   const [selectedID, setSelectedID] = React.useState(null)
   const [shadow, setShadow] = React.useState(false)
@@ -281,9 +219,16 @@ const App = () => {
   const handleDrop = (e) => {
 
     if (e.target.x() < 400) {
-      let stateCopy = cirkows.slice()
-      stateCopy.push([e.target.x(), e.target.y()])
-      setCirkows(stateCopy)
+
+      let stateCopy = nodes.slice()
+      stateCopy.push({
+        x: e.target.x(),
+        y: e.target.y(),
+        radius: 10,
+        fill: 'chartreuse',
+        id: nodes.length+1
+      })
+      setNodes(stateCopy)
     }
 
   }
@@ -307,7 +252,7 @@ const App = () => {
         setMoreShapes(stateCopy)
         setArrowDrawIdStash(null)
       }
-   }
+    }
   }
 
   const handleArrowDrawModeToggle = () => {
@@ -337,16 +282,17 @@ const App = () => {
         <p>Hi there <span role="img" aria-label="upside-down smiley face">🙃</span></p>
         <Stage onMouseDown={checkDeselect} onTouchStart={checkDeselect} width={500} height={500} style={{border: '2px solid deeppink'}}>
           <Layer>
+            {/* demonstrating ddrag and drop*/}
             <Circle radius={15} fill='deeppink' x={420} y={420} draggable={true} onDragEnd={(e) => {handleDrop(e)}} />
-          </Layer>
-          <Layer>
+            {/* demonstrating HTML elements */}
             <Html divProps={{style:{position: 'relative', top: 400, left: 400}}}>
               <input type="range" min="-10" max="10" value={value} onChange={(e) => {handleSlider(e)}}></input>
               <button onClick={() => {handleArrowDrawModeToggle()}}>Arrow Draw Mode: {arrowDrawMode.toString()}</button>
             </Html>
+            {/* mapping data */}
             {nodes.map((node, i) => {
               return(
-                <CircleComp
+                <Node
                   key={i}
                   shapeProps={node}
                   isSelected={node.id === selectedID}
@@ -359,40 +305,8 @@ const App = () => {
                 />
               )
             })}
-            {rectangles.map((rect, i) => {
-              return(
-                <Rectangle
-                  key={i}
-                  shapeProps={rect}
-                  isSelected={rect.id === selectedID}
-                  onSelect={() => {setSelectedID(rect.id)}}
-                  onChange={(newAttrs) => {
-                    const rects = rectangles.slice();
-                    rects[i] = newAttrs
-                    setRectangles(rects) 
-                  }}
-                />)
-            })}
-            <Group
-              draggable={true}>
-              <Ellipse
-                radius={{
-                  x: 50,
-                  y: 20,
-                }}
-                x={220}
-                y={20}
-                fill='deeppink'
-              />
-              <Text
-                x={220}
-                y={20}
-                fill='black'
-                fontFamily='Courier New'
-                text='yo!'
-              />
-            </Group>
-            {cirkows.map((cirkow) => <Circle width={15} height={15} fill='deeppink' x={cirkow[0]} y={cirkow[1]} /> )}
+
+            {/* demonstrating autocreation of arrows */}
             {moreShapes.map((shape, i) => {return(
               <React.Fragment>
                 <Circle id={shape.id.toString()} width={15} onClick={(e) => {if(arrowDrawMode) {handleCircleSelect(e)}}} height={15} fill='orange' x={shape.x} y={shape.y} /> 
@@ -404,6 +318,7 @@ const App = () => {
             )}
             )}
 
+            {/* illustrating arrow select, smart arrows, arrow tension/ediitng*/}
             <Circle radius={20} fill='cadetblue' draggable={true} x={c1[0]} y={c1[1]} onDragMove={(e) => {setC1([e.target.x(), e.target.y()])}}/>
             <Circle radius={20} fill='cadetblue' draggable={true} x={c2[0]} y={c2[1]} onDragMove={(e) => {setC2([e.target.x(), e.target.y()])}}/>
             <Circle radius={20} fill='cadetblue' draggable={true} x={c3[0]} y={c3[1]} onDragMove={(e) => {setC3([e.target.x(), e.target.y()])}}/>
